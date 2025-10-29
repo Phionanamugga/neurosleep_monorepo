996ebfbef13e0288158c3444a925093c6d4c5316
Phionanamugga <phionanamugga15@gmail.com>
2025-10-29 22:18:25 +0100

    modifies the url file

--- backend-django/neurosleep_backend/urls.py
+++ backend-django/neurosleep_backend/urls.py
@@ -1,4 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+from sleep.views import home  # import your home view
+
+
 urlpatterns = [
-    path("api/v1/", include("sleep.urls")),
+    path('admin/', admin.site.urls),
+    path('api/', include('sleep.urls')),  # ✅ All your model endpoints live here
+    path('', home, name='home'),        # render index.html here
 ]
+
